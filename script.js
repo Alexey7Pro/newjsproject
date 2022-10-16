0da2c04c8827c65bd6ed6ad37d38891e862dd07d
geekbrains <info@geekbrains.ru>
2022-10-16 12:35:27 +0500

    My lesson6

--- script.js
+++ script.js
@@ -11,39 +11,110 @@ catalogData.json`
 const GET_BASKET_GOODS_ITEMS = `${BASE_URL}
 getBasket.json`;
 
-function service(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onload = () => {
-    if (xhr.readyState === 4) {
-      callback(JSON.parse(xhr.response))
-    }
-  }
+function service(url) {
+  return fetch(url)
+    .then((res) => res.json())
 }
 
-const app = new Vue({
-  el: '#root',
-  data: {
-    goods: [],
-    search: ''
-  },
-  mounted() {
-    service(GET_GOODS_ITEMS, (data) => {
-      this.goods = data;
-    });
-  },
-  computed: {
-    calculatePrice() {
-      return this.goods.reduce((prev, { price }) => {
-        return prev + price;
-      }, 0)
+function init() {
+
+
+  Vue.component('newsearch', {
+
+    template: `
+    <input type="text" class="goods-search" v-model="search"  @click="$emit('input')"/>
+                `
+  });
+
+  Vue.component('basket', {
+
+    template: `
+    <div class="fixed-area">
+    <div class="basket-card">
+        <div class="basket-card__header">
+            <h1 class="basket-card__header__title">basket card</h1>
+            <div class="basket-card__header__delete-icon" @click="$emit('close')"></div>
+        </div>
+        <div class="basket-card__content">content</div>
+    </div>
+</div>
+    `
+  });
+
+
+  Vue.component('custom_button', {
+
+    template: `
+    <button class="cart-button" type="button" v-on: @click="$emit('click')">
+    <slot></slot>
+    </button>
+    `
+  });
+
+
+
+  Vue.component('goods_item', {
+    props: [
+      'item'
+    ],
+    template: `
+      <div class = "goods-item" >
+                    <h3>{{ item.product_name }}</h3>
+    <p>{{ item.price }}</p>
+                    </div>
+    `
+  })
+
+
+
+
+
+  const app = new Vue({
+    el: '#root',
+    data: {
+      items: [],
+      filteredItems: [],
+      search: '',
+      isVisibleCart: false
     },
-    filteredGoods() {
-      return this.goods.filter((item) => {
-        const regExp = new RegExp(this.search);
-        return regExp.test(item.product_name)
-      })
+    methods: {
+      fetchGoods() {
+        service(GET_GOODS_ITEMS).then((data) => {
+          this.items = data;
+          this.filteredItems = data;
+        });
+      },
+      filterItems() {
+        this.filteredItems = this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
+      },
+
+      setVisibleCard() {
+        this.isVisibleCart = !this.isVisibleCart;
+      }
+    },
+    computed: {
+      calculatePrice() {
+        return this.filteredItems.reduce((prev, { price }) => {
+          return prev + price;
+        }, 0)
+      }
+    },
+
+    mounted() {
+      this.fetchGoods();
     }
-  }
-})
+  })
+}
+window.onload = init
+
+
+
+
+
+
+
+
+
+
